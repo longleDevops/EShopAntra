37b9188ccf1ecc141a38db3fa75a0d93e5ab762e
lethienlong <long.le.devops@gmail.com>
2024-07-29 07:22:11 -0700

    .

--- src/app/core/interceptors/jwt.interceptor.ts
+++ src/app/core/interceptors/jwt.interceptor.ts
@@ -3,7 +3,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem("token");
   if (token){
-    request = 
+    
   }
   return next(req);
 };
